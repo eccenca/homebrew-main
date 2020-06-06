4cf1263f46d65d298bb7156df343fc164948810a
Sebastian Tramp <sebastian.tramp@eccenca.com>
2020-06-06 18:22:58 +0200

    add cmemc formula

---
+++ Formula/cmemc.rb
@@ -0,0 +1,21 @@
+class Cmemc < Formula
+  desc "eccenca Corporate Memory Command Line Interface (CLI)"
+  homepage "https://eccenca.com/go/cmemc"
+  url "https://releases.eccenca.com/cmemc/cmemc-v20.06.tgz"
+  sha256 "9390f0c41cb647055fe57421b27fd3948273c997f6e3f834b130cf5ec5842799"
+
+  def install
+    rm Dir["cmemc"]
+    rm Dir["cmemc.exe"]
+    rm Dir["cmemc-rhel"]
+    rm Dir["*.pdf"]
+    libexec.install Dir["*"]
+    bin.install_symlink "#{libexec}/cmemc-macosx" => "cmemc"
+  end
+
+  test do
+    # no tests atm
+    system "true"
+  end
+end
+
