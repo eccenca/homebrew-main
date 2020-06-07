24c22ab9019912566a76f98738cb08e0f15fe32c
Sebastian Tramp <sebastian.tramp@eccenca.com>
2020-06-07 11:29:41 +0200

    fix test-bot issues

--- Formula/cmemc.rb
+++ Formula/cmemc.rb
@@ -1,13 +1,13 @@
 class Cmemc < Formula
-  desc "eccenca Corporate Memory Command Line Interface (CLI)"
+  desc "The eccenca Corporate Memory Command-Line Interface (CLI)"
   homepage "https://eccenca.com/go/cmemc"
   url "https://releases.eccenca.com/cmemc/cmemc-v20.06.tgz"
   sha256 "9390f0c41cb647055fe57421b27fd3948273c997f6e3f834b130cf5ec5842799"
 
   def install
-    rm Dir["cmemc"]
-    rm Dir["cmemc.exe"]
-    rm Dir["cmemc-rhel"]
+    rm "cmemc"
+    rm "cmemc.exe"
+    rm "cmemc-rhel"
     rm Dir["*.pdf"]
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/cmemc-macosx" => "cmemc"
@@ -18,4 +18,3 @@ class Cmemc < Formula
     system "true"
   end
 end
-
